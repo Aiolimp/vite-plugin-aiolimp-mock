a39e05a15309f8c0af73eea61a6698a7850310e0
BaoYu <baoyu@for-change.cn>
2025-04-18 17:18:14 +0800

    fix: exports and entry settings

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from 'vite';
-
-interface MockOptions {
-    mockDir?: string;
-    delay?: number;
-}
-declare function vitePluginSimpleMock(options?: MockOptions): Plugin;
-
-export { type MockOptions, vitePluginSimpleMock as default };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'], // 插件入口
-  format: ['esm', 'cjs'], // 同时打包为 ESModule 和 CommonJS
-  dts: true, // 生成 index.d.ts
-  splitting: false, // 不进行代码分割（插件不需要）
-  sourcemap: false, // 是否需要 sourcemap，看你是否调试需要
-  clean: true, // 构建前清除 dist
-  outDir: 'dist', // 输出目录
-  target: 'esnext', // 保留现代语法
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  outDir: 'dist',
 });
